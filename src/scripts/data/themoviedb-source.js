05d201cbc8bef96949dadddd270f6f5656c072f3
ArinBisa123 <arrinikhirofati17@gmail.com>
2023-01-04 14:49:14 +0700

    fixed code

--- src/scripts/data/themoviedb-source.js
+++ src/scripts/data/themoviedb-source.js
@@ -4,13 +4,13 @@ class TheMovieDbSource {
   static async nowPlayingMovies() {
     const response = await fetch(API_ENDPOINT.NOW_PLAYING);
     const responseJson = await response.json();
-    return responseJson.result;
+    return responseJson.results;
   }
 
   static async upcomingMovies() {
     const response = await fetch(API_ENDPOINT.UPCOMING);
     const responseJson = await response.json();
-    return responseJson.result;
+    return responseJson.results;
   }
 
   static async detailMovie(id) {
