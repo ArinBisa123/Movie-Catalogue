2470d59564d72038c0bc30f71d77275bae27e405
ArinBisa123 <arrinikhirofati17@gmail.com>
2023-01-10 10:41:53 +0700

    ad new style to style.css, add createLikeButtonTemplate & createLikedButtonTemplate function to template-creator.js, edit detail.js

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,17 +1,20 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parser';
-import { createMovieDetailTemplate } from '../templates/template-creator';
+import { createLikeButtonTemplate, createMovieDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
     <div id="movies" class="movies"> </div>
+    <div id="likeButtonContainer> </div>
     `;
   },
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const movie = await TheMovieDbSource.detailMovie(url.id);
     const moviesContainer = document.querySelector('#movies');
+    const likeButtonContainer=document.querySelector('#likeButtonContainer');
+    likeButtonContainer.innerHTML = createLikeButtonTemplate();
     moviesContainer.innerHTML = createMovieDetailTemplate(movie);
   },
 };
