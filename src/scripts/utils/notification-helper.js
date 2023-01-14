0761f559cf2e9fba4c253c53ee5eef825b7eb2fb
ArinBisa123 <arrinikhirofati17@gmail.com>
2023-01-15 00:28:41 +0700

    change and add some functions

--- src/scripts/utils/notification-helper.js
+++ src/scripts/utils/notification-helper.js
@@ -1,35 +1,39 @@
 const NotificationHelper = {
   sendNotification({ title, options }) {
-    // Check Availability
-    if (!this._checkAvailabillity()) {
+    if (!this._checkAvailability()) {
       console.log('Notification not supported in this browser');
       return;
     }
-    // Check Permissions
+
     if (!this._checkPermission()) {
       console.log('User did not yet granted permission');
       this._requestPermission();
       return;
     }
-    // Show Notification
+
     this._showNotification({ title, options });
   },
-  _checkAvailabillity() {
+
+  _checkAvailability() {
     return 'Notification' in window;
   },
+
   _checkPermission() {
     return Notification.permission === 'granted';
   },
+
   async _requestPermission() {
     const status = await Notification.requestPermission();
 
     if (status === 'denied') {
       console.log('Notification Denied');
     }
+
     if (status === 'default') {
-      console.log('Permission Closed');
+      console.log('Permission closed');
     }
   },
+
   async _showNotification({ title, options }) {
     const serviceWorkerRegistration = await navigator.serviceWorker.ready;
     serviceWorkerRegistration.showNotification(title, options);
