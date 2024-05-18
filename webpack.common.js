fc23bf130be4a6cdb250556916922b31d98316d0
ArinBisa123 <leejindairy06@gmail.com>
2024-05-18 07:31:23 +0700

    add environment variable, reconfigure webpack, jest, and yaml

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+require('dotenv').config({
+  path: path.resolve('.env'),
+});
 
 module.exports = {
   entry: {
